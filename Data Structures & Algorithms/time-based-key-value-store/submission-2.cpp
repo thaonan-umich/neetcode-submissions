9ab665cdf973f73f4f87e0733380b752bec944eb
Nathan Tang <thaonan@umich.edu>
2026-05-27 11:05:39 +0800

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.cpp
@@ -0,0 +1,44 @@
+class TimeMap {
+private:
+    unordered_map<string, vector<pair<int, string>>> db;
+
+public:
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        db[key].push_back({timestamp, value});  // c++ unordered_map 没命中会自己创建
+    }
+    
+    string get(string key, int timestamp) {
+        if (db.find(key) == db.end())
+            return "";
+        
+        const auto& pairs_vec = db[key];
+
+        int left = 0;
+        int right = pairs_vec.size() - 1;
+        string res = "";
+
+        // 开始二分查找
+        // 二分思想：二分剪枝榨干所有可能区域
+
+        while (left <= right){
+            int mid = (left + right) / 2;
+
+            if (pairs_vec[mid].first > timestamp){
+                right = mid - 1;
+            }
+            else{   // pairs_vec[mid].first <= timestamp
+                // 这是一个OK的答案，先存着
+                res = pairs_vec[mid].second;
+
+                // 继续去可能更大的右半部分找
+                left = mid + 1;
+            }
+        }
+
+        return res;
+    }
+};
