b7bec98a7ccc63b22e13e6ca1cf9224ded92c681
Nathan Tang <thaonan@umich.edu>
2026-08-08 20:28:07 +0800

    Add: gas-station - submission-3

---
+++ Data Structures & Algorithms/gas-station/submission-3.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        
+        int totalGas = 0;
+        int totalCost = 0;
+
+        for (int i = 0; i < gas.size(); ++i) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+        }
+
+        if (totalGas < totalCost) {
+            return -1;
+        }
+
+        int contain = 0;
+        int start = 0;
+
+        for (int i = 0; i < gas.size(); ++i) {
+            
+            contain += (gas[i] - cost[i]);
+
+            if (contain < 0) {
+                contain = 0;
+                start = i + 1;
+            }
+        }
+
+        return start;
+    }
+};
